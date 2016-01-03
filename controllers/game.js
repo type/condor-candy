06cb831491b56192b4c0c0de385dfe04414877f0
Guillaume Goussard <guillaume.goussard@mgo.com>
2016-01-03 04:50:24 -0800

    re-add icons

--- controllers/game.js
+++ controllers/game.js
@@ -2,7 +2,7 @@ var rules = require('../lib/rules');
 
 
 module.exports = function(req, res) {
-    rules.checkPoints(function(err, points) {
+    rules.checkPoints(function(err, rules) {
 
         var points = require('../lib/points'),
         animals = require('../lib/animals'),
@@ -12,9 +12,9 @@ module.exports = function(req, res) {
             animals: animals.animals,
             myPoints: points.getPoints(),
             providerIncentives: [
-            {providerName: "Socal Edison", description: "Winter Wonder: 10% off your next billing period", requirements: "Set your heat to 68 or below", value: points.heatRule},
-            {providerName: "Socal Edison", description: "Summer Saver: 10% off your next billing period", requirements: "Set your air conditioner to 78 or above", value: points.acRule},
-            {providerName: "Socal Edison", description: "Insider Deal: 10% off your next billing period", requirements: "Ensure windows and exterior doors are closed when using your HVAC system", value: points.windowRule},
+            {providerName: "Socal Edison", description: "Winter Wonder: 10% off your next billing period", requirements: "Set your heat to 68 or below", value: rules.heatRule},
+            {providerName: "Socal Edison", description: "Summer Saver: 10% off your next billing period", requirements: "Set your air conditioner to 78 or above", value: rules.acRule},
+            {providerName: "Socal Edison", description: "Insider Deal: 10% off your next billing period", requirements: "Ensure windows and exterior doors are closed when using your HVAC system", value: rules.windowRule},
             {providerName: "DWP", description: "15% off water bill next billing period", requirements: "Save laundry and dishwasher loads until non-peak hours"}
 
             ],
