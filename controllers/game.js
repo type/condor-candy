f9415e4e365ae70c49677aec60e322aa0f4a0bf4
Guillaume Goussard <guillaume.goussard@mgo.com>
2016-01-03 04:02:10 -0800

    add rules to the pages

--- controllers/game.js
+++ controllers/game.js
@@ -1,19 +1,24 @@
+var rules = require('../lib/rules');
+
+
 module.exports = function(req, res) {
-    var data = {
-        myPoints: 1000,
-        providerIncentives: [
-        {providerName: "Socal Edison", description: "Winter Wonder: 10% off your next billing period", requirements: "Set your heat to 68 or below"},
-        {providerName: "Socal Edison", description: "Summer Saver: 10% off your next billing period", requirements: "Set your air conditioner to 78 or above"},
-        {providerName: "Socal Edison", description: "Insider Deal: 10% off your next billing period", requirements: "Ensure windows and exterior doors are closed when using your HVAC system"},
-        {providerName: "DWP", description: "15% off water bill next billing period", requirements: "Save laundry and dishwasher loads until non-peak hours"}
-        ],
-        rewardTiers: [
-        {animal: "panda", name: "Panda", img: "bamboo.png", description: "Bamboo is sure to lure the elusive giant panda", points: 200},
-        {animal: "condor", name: "condor", img: "meat.png", description: "Carrion is candy for condors.", points: 500},
-        {animal: "quokka", name: "Quokka", img: "grasses.png", description: "Grass will make the quokka go crackers." , points: 800}
-        ],
-        contests: [{description: "Win a night at the Wild Animal Park"}
-        ]
-    }
-    res.render('templates/game/main', data);
+    rules.checkPoints(function(err, points) {
+        var data = {
+            w: 1000,
+            providerIncentives: [
+            {providerName: "Socal Edison", description: "Winter Wonder: 10% off your next billing period", requirements: "Set your heat to 68 or below", value: points.heatRule},
+            {providerName: "Socal Edison", description: "Summer Saver: 10% off your next billing period", requirements: "Set your air conditioner to 78 or above", value: points.acRule},
+            {providerName: "Socal Edison", description: "Insider Deal: 10% off your next billing period", requirements: "Ensure windows and exterior doors are closed when using your HVAC system", value: points.windowRule},
+                {providerName: "DWP", description: "15% off water bill next billing period", requirements: "Save laundry and dishwasher loads until non-peak hours"}
+            ],
+            rewardTiers: [
+            {animal: "panda", name: "Panda", img: "bamboo.png", description: "Bamboo is sure to lure the elusive giant panda", points: 200},
+            {animal: "condor", name: "condor", img: "meat.png", description: "Carrion is candy for condors.", points: 500},
+            {animal: "quokka", name: "Quokka", img: "grasses.png", description: "Grass will make the quokka go crackers." , points: 800}
+            ],
+            contests: [{description: "Win a night at the Wild Animal Park"}
+            ]
+        }
+        res.render('templates/game/main', data);
+    });
 }
